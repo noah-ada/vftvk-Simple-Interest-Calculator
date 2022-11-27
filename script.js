bbc878688bf55d5daed58bc6463e497ed238237b
noah-ada <eric.noahada@student.unamur.be>
2022-11-27 14:21:48 -0500

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
     
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        document.getElementById("result").innerHTML = "";
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value; 
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var computInterest= "if you deposit "+ principal + ", <br/>" +
+                        "at an interest rate of " + rate + "%. <br/>" +
+                        "You will receive an amount of "+interest+",<br/>"+
+                        "in the year "+ year;
+    document.getElementById("result").innerHTML = computInterest;
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
